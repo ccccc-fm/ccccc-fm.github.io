55cbb57752717d014363b1c9853e6dfaad56a320
ccccc-fm <cccc_fm@163.com>
2022-11-16 00:48:53 +0800

    Site updated: 2022-11-16 00:48:53

--- js/main.js
+++ js/main.js
@@ -1,107 +1,139 @@
-// 菜单
-$(".menu-switch").click(function () {
-    if ($(this).hasClass('icon-menu-outline')) {
-        $(this).removeClass(' icon-menu-outline ').addClass('icon-close-outline');
-        $('.menu-container').css('opacity', '1').css('height', 'auto');
-    } else {
-        $(this).addClass(' icon-menu-outline ').removeClass('icon-close-outline');
-        $('.menu-container').css('opacity', '0');
-
-        var that = $(this);
-        setTimeout(function () {that.hasClass('icon-menu-outline') && $('.menu-container').css('height', '0')}, 600)
-    }
-});
-
-if (window.is_post) {
-    // 图片放大
-    $(".post-detail img").each(function() {
-        var currentImage = $(this);
-        currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-fancybox='lightbox' data-caption='" + currentImage.attr("alt") + "'></a>");
+/* eslint-disable node/no-unsupported-features/node-builtins */
+(function($, moment, ClipboardJS, config) {
+    $('.article img:not(".not-gallery-item")').each(function() {
+        // wrap images with link and add caption if possible
+        if ($(this).parent('a').length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            if (this.alt) {
+                $(this).after('<p class="has-text-centered is-size-6 caption">' + this.alt + '</p>');
+            }
+        }
     });
 
-    // 代码复制
-    var $copyIcon = $('<i class="fa-solid icon icon-copy copy-code" title="复制代码"></i>');
-    $(".post-detail figure").append($copyIcon);
-    $(".post-detail pre[class*=language-].line-numbers").append($copyIcon);
-    $('.post-detail .copy-code').on('click', function () {
-        var selection = window.getSelection();
-        var range = document.createRange();
-        var table = $(this).prev('table');
-        if (table.length) {
-            range.selectNodeContents($(this).prev('table').find('.code').find('pre')[0]);
+    if (typeof $.fn.lightGallery === 'function') {
+        $('.article').lightGallery({ selector: '.gallery-item' });
+    }
+    if (typeof $.fn.justifiedGallery === 'function') {
+        if ($('.justified-gallery > p > .gallery-item').length) {
+            $('.justified-gallery > p > .gallery-item').unwrap();
+        }
+        $('.justified-gallery').justifiedGallery();
+    }
+
+    if (typeof moment === 'function') {
+        $('.article-meta time').each(function() {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
+    }
+
+    $('.article > .content > table').each(function() {
+        if ($(this).width() > $(this).parent().width()) {
+            $(this).wrap('<div class="table-overflow"></div>');
+        }
+    });
+
+    function adjustNavbar() {
+        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
+        if ($(document).outerWidth() < navbarWidth) {
+            $('.navbar-main .navbar-menu').addClass('justify-content-start');
         } else {
-            console.log($(this).prev('code')[0]);
-            range.selectNodeContents($(this).prev('code')[0]);
+            $('.navbar-main .navbar-menu').removeClass('justify-content-start');
+        }
+    }
+    adjustNavbar();
+    $(window).resize(adjustNavbar);
+
+    function toggleFold(codeBlock, isFolded) {
+        const $toggle = $(codeBlock).find('.fold i');
+        !isFolded ? $(codeBlock).removeClass('folded') : $(codeBlock).addClass('folded');
+        !isFolded ? $toggle.removeClass('fa-angle-right') : $toggle.removeClass('fa-angle-down');
+        !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
+    }
+
+    function createFoldButton(fold) {
+        return '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
+    }
+
+    $('figure.highlight table').wrap('<div class="highlight-body">');
+    if (typeof config !== 'undefined'
+        && typeof config.article !== 'undefined'
+        && typeof config.article.highlight !== 'undefined') {
+
+        $('figure.highlight').addClass('hljs');
+        $('figure.highlight .code .line span').each(function() {
+            const classes = $(this).attr('class').split(/\s+/);
+            if (classes.length === 1) {
+                $(this).addClass('hljs-' + classes[0]);
+                $(this).removeClass(classes[0]);
+            }
+        });
+
+
+        const clipboard = config.article.highlight.clipboard;
+        const fold = config.article.highlight.fold.trim();
+
+        $('figure.highlight').each(function() {
+            if ($(this).find('figcaption').length) {
+                $(this).find('figcaption').addClass('level is-mobile');
+                $(this).find('figcaption').append('<div class="level-left">');
+                $(this).find('figcaption').append('<div class="level-right">');
+                $(this).find('figcaption div.level-left').append($(this).find('figcaption').find('span'));
+                $(this).find('figcaption div.level-right').append($(this).find('figcaption').find('a'));
+            } else {
+                if (clipboard || fold) {
+                    $(this).prepend('<figcaption class="level is-mobile"><div class="level-left"></div><div class="level-right"></div></figcaption>');
+                }
+            }
+        });
+
+        if (typeof ClipboardJS !== 'undefined' && clipboard) {
+            $('figure.highlight').each(function() {
+                const id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
+                const button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
+                $(this).attr('id', id);
+                $(this).find('figcaption div.level-right').append(button);
+            });
+            new ClipboardJS('.highlight .copy'); // eslint-disable-line no-new
         }
 
-        selection.removeAllRanges();
-        selection.addRange(range);
-        var text = selection.toString();
-        document.execCommand('copy');
-        selection.removeAllRanges();
+        if (fold) {
+            $('figure.highlight').each(function() {
+                $(this).addClass('foldable'); // add 'foldable' class as long as fold is enabled
 
-        $(this).html('<span class="copy-success"> 复制成功</span>');
-        setTimeout(() => {
-            $(this).html('');
-        }, 2500)
-    });
-
-    // 代码语言
-    $(function () {
-        $('code').each(function () {
-            var code_language = $(this).attr('class');
-
-            if (!code_language) {
-                return true;
-            }
-            var lang_name = code_language.replace("line-numbers", "").trim().replace("highlight", "").trim().replace("language-", "").trim();
-
-            $(this).attr('data-content-after', lang_name || 'CODE');
-        });
-        $('.highlight').each(function () {
-            var code_language = $(this).attr('class');
-
-            if (!code_language) {
-                return true;
-            }
-            var lang_name = code_language.replace("highlight", "").trim();
-
-            $(this).attr('data-content-after', lang_name || 'CODE');
-        });
-    });
-
-    // 文章详情侧边目录
-    let mainNavLinks = document.querySelectorAll(".top-box a");
-    window.addEventListener("scroll", event => {
-        let fromTop = window.scrollY + 100;
-
-        mainNavLinks.forEach((link, index) => {
-            let section = document.getElementById(decodeURI(link.hash).substring(1));
-            let nextSection = null
-            if (mainNavLinks[index + 1]) {
-                nextSection = document.getElementById(decodeURI(mainNavLinks[index + 1].hash).substring(1));
-            }
-            if (section.offsetTop <= fromTop) {
-                if (nextSection) {
-                    if (nextSection.offsetTop > fromTop) {
-                        link.classList.add("current");
-                    } else {
-                        link.classList.remove("current");
+                if ($(this).find('figcaption').find('span').length > 0) {
+                    const span = $(this).find('figcaption').find('span');
+                    if (span[0].innerText.indexOf('>folded') > -1) {
+                        span[0].innerText = span[0].innerText.replace('>folded', '');
+                        $(this).find('figcaption div.level-left').prepend(createFoldButton('folded'));
+                        toggleFold(this, true);
+                        return;
                     }
-                } else {
-                    link.classList.add("current");
                 }
-            } else {
-                link.classList.remove("current");
-            }
-        });
-    });
+                $(this).find('figcaption div.level-left').prepend(createFoldButton(fold));
+                toggleFold(this, fold === 'folded');
+            });
 
-    // 点击锚点滚动条偏移
-    $(".top-box-link").click(function(){
-        setTimeout(function () {
-            $(window).scrollTop($(window).scrollTop() - 54);
-            console.log($(window).scrollTop() - 54, '滚动条位置');
-        }, 0);
-    });
-}
+            $('figure.highlight figcaption .level-left').click(function() {
+                const $code = $(this).closest('figure.highlight');
+                toggleFold($code.eq(0), !$code.hasClass('folded'));
+            });
+        }
+    }
+
+    const $toc = $('#toc');
+    if ($toc.length > 0) {
+        const $mask = $('<div>');
+        $mask.attr('id', 'toc-mask');
+
+        $('body').append($mask);
+
+        function toggleToc() { // eslint-disable-line no-inner-declarations
+            $toc.toggleClass('is-active');
+            $mask.toggleClass('is-active');
+        }
+
+        $toc.on('click', toggleToc);
+        $mask.on('click', toggleToc);
+        $('.navbar-main .catalogue').on('click', toggleToc);
+    }
+}(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
